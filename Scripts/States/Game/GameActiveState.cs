332a8fed651ae7be66fdbb24ec384a14118252e0
Dee Pee <dave.pond+gitkraken@gmail.com>
2020-03-21 21:23:25 -0400

    Pause screen

--- Scripts/States/Game/GameActiveState.cs
+++ Scripts/States/Game/GameActiveState.cs
@@ -1,7 +1,20 @@
 using Godot;
 using System;
+using InvasionRPG.Scripts.Enums;
 
 public class GameActiveState : GameState
 {
-    
+    public override void Enter()
+    {
+        GetTree().Paused = false;
+    }
+
+    public override void HandleInput(InputEvent inputEvent)
+    {
+        base.HandleInput(inputEvent);
+        
+        if (inputEvent is InputEventKey key && key.Pressed)
+            if (Input.IsActionPressed("ui_cancel"))
+                StateMachine.ChangeState<GamePausedState>(StateTypes.Game);
+    }
 }
--- Scripts/States/Game/GameState.cs
+++ Scripts/States/Game/GameState.cs
@@ -4,4 +4,7 @@ using System;
 public class GameState : State
 {
     protected GameController Game => Parent as GameController;
+    protected StateMachine StateMachine => Game.StateMachine;
+    protected Label GameStateLabel => Parent.GetNode<Label>("GameUI/GameStateLabel");
+    
 }
